bd6bf2e2c2e5b4b5584fb4f5979a590c3a8d37b7
yuyun <yunyastutik@gmail.com>
2020-10-08 17:29:46 +0700

    finishing server

--- cloning-github/server/src/controllers/github.controller.js
+++ cloning-github/server/src/controllers/github.controller.js
@@ -1,27 +1,91 @@
 const axios = require('axios');
 
 const instance = axios.create({
-    baseURL: 'https://github.com/'
+    baseURL: 'https://api.github.com/'
 });
-
 
 class githubCloneController {
     static getToken(req, res) {
 
-        instance.post('login/oauth/access_token', null,{params:{
-            client_id : process.env.CLIENT_ID,
+        axios.post('https://github.com/login/oauth/access_token', {
+            client_id: process.env.CLIENT_ID,
             client_secret: process.env.CLIENT_SECRET,
             code: req.params.code
-        }})
+        })
             .then((data) => {
-                res.status(200).json(data)
+                res.status(data.status).json(
+                    data.data
+                )
             })
-            .catch((err)=>{
+            .catch((err) => {
                 res.status(404).json({
+                    message: err.message
+                })
+            })
+    }
+
+    static getUser(req, res) {
+        instance.get('user', {
+            headers: { 'Authorization': `token ${req.headers.token}` }
+        })
+            .then(({ data }) => {
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                res.status(err.status).json({
+                    message: err.message
+
+                })
+            })
+    }
+
+    static getRepository(req, res) {
+        instance.get('user/repos', {
+            headers: { 'Authorization': `token ${req.headers.token}` }
+        })
+            .then(({ data }) => {
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                console.log(err)
+                res.status(err.status).json({
+                    message: err.message
+
+                })
+            })
+    }
+
+    static newRepository(req, res) {
+        instance.post('user/repos', {
+            name: req.body.name,
+            description: req.body.description,
+            private: req.body.private,
+            auto_init: req.body.auto_init
+        }, {
+            headers: { 'Authorization': `token ${req.headers.token}` }
+        })
+            .then(({ data }) => {
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                res.status(err.status).json({
+                    message: err.message
+
+                })
+            })
+    }
+
+    static deleteRepository(req, res) {
+        instance.delete(`repos/${req.params.owner}/${req.params.repo}`, {
+            headers: { 'Authorization': `token ${req.headers.token}` }
+        })
+            .then()
+            .catch((err) => {
+                res.status(403).json({
                     message: err.message
                 })
             })
     }
 }
 
-module.exports=githubCloneController;
+module.exports = githubCloneController;
--- cloning-github/server/src/routers/github.router.js
+++ cloning-github/server/src/routers/github.router.js
@@ -2,6 +2,15 @@ const router = require('express').Router();
 
 const githubController = require('../controllers/github.controller');
 
+router.get('/user', githubController.getUser);
+
+router.get('/getRepo', githubController.getRepository);
+
 router.get('/:code', githubController.getToken);
+
+router.post('/', githubController.newRepository);
+
+router.delete('/:owner/:repo', githubController.deleteRepository);
+
 
 module.exports=router;
